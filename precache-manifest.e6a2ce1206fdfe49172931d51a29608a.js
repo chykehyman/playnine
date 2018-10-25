309ba4ae7712567393633e677f4d2e8a594092c3
Andela <andela@Andelas-MacBook-Pro-4.local>
2018-10-25 11:58:12 +0100

    Updates

---
+++ precache-manifest.e6a2ce1206fdfe49172931d51a29608a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf95e092cd12509e8e87",
+    "url": "/chykehyman/playnine/static/js/runtime~main.bf95e092.js"
+  },
+  {
+    "revision": "1863276f6c4124b3febb",
+    "url": "/chykehyman/playnine/static/js/main.1863276f.chunk.js"
+  },
+  {
+    "revision": "564aff718091464665fb",
+    "url": "/chykehyman/playnine/static/js/1.564aff71.chunk.js"
+  },
+  {
+    "revision": "1863276f6c4124b3febb",
+    "url": "/chykehyman/playnine/static/css/main.49905001.chunk.css"
+  },
+  {
+    "revision": "5c17e65e27e0c99a42f5d58f6e060851",
+    "url": "/chykehyman/playnine/index.html"
+  }
+];
